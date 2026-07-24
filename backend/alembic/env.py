4fe9820dfeca63883a86d4c45df5524559c2757b
李志渊 <zhiyuan.li@sucheon.com>
2026-07-24 17:48:29 +0800

    psycopg2

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,7 +8,7 @@ from app.models import Base
 
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("+asyncpg", ""))
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("+asyncpg", "+psycopg"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
